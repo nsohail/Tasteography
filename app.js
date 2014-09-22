85150ee78a4e0cfd509d98b4d108885c46dcb33a
nsohail <nsohail92@gmail.com>
2014-09-22 01:00:53 -0400

    test

--- app.js
+++ app.js
@@ -178,10 +178,11 @@ function callback(results, status){
         //var contentString = '<IMG BORDER="0" ALIGN="Left" SRC="'+photoForPlace+'">'+'<div class="name">"'+this.name+'"</div>';
 
         google.maps.event.addListener(marker, 'click', function() {
-
-            
-            infowindow.setContent('<img align="left" width="100px" src="'+this.photo+'">' + this.name + '<br>"'+this.location+'"');
-    
+            if((!this.photo) || (this.photo === undefined)){
+            infowindow.setContent('<img align="left" width="100px" src="'+this.photo+'">' + this.name + '<br>'+this.location+'');
+            }else{
+            infowindow.setContent('<img align="left" width="100px" src="'+this.photo+'">' + this.name + '<br>'+this.location+'');
+            }
             infowindow.open(map, this);
         });
     
