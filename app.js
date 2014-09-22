e7ec51aa9d0118c96b33c85670736ad725f843b5
nsohail <nsohail92@gmail.com>
2014-09-22 01:40:02 -0400

    test

--- app.js
+++ app.js
@@ -181,7 +181,7 @@ function callback(results, status){
             if((!this.photo) || (this.photo === undefined)){
             infowindow.setContent('<img class="windowPic" align="left" width="100px" src="http://www.uwplatt.edu/files/styles/high_resolution/public/image_fields/directory_image/image-not-available_1.jpg?itok=GIB8RUHy">' + '<div class="windowName"> '+ this.name +' </div>' + '<br>'+ '<div class="windowLocation"> '+this.location+' </div>');
             }else{
-            infowindow.setContent('<img class="windowPic" align="left" width="100px" src="'+this.photo+'">' + this.name + '<br>' + '<div class="windowLocation"> '+this.location+' </div>');
+            infowindow.setContent('<img class="windowPic" align="left" width="100px" src="'+this.photo+'">' + '<div class="windowName"> '+ this.name +' </div>' + '<br>' + '<div class="windowLocation"> '+this.location+' </div>');
             }
             infowindow.open(map, this);
         });
