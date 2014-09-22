b12492e1c8605c5e9a1f7842578fc10e3e8b937a
nsohail <nsohail92@gmail.com>
2014-09-21 23:54:15 -0400

    test

--- app.js
+++ app.js
@@ -178,7 +178,7 @@ function callback(results, status){
         //var contentString = '<IMG BORDER="0" ALIGN="Left" SRC="'+photoForPlace+'">'+'<div class="name">"'+this.name+'"</div>';
 
         google.maps.event.addListener(marker, 'click', function() {
-            infowindow.setContent(this.name + '<img src="'+miniPhoto+'">');
+            infowindow.setContent(this.name + '<img src="'+this.icon+'">');
             infowindow.open(map, this);
         });
     
